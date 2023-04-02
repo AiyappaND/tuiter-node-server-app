849ed8433da2290a40a6f9c412f01d9315fe3f9d
AiyappaND <vinayaiyappa24816@gmail.com>
2023-04-01 22:08:56 -0400

    Add tuit controllers and CRUD ops

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ import HelloController
     from "./controllers/hello-controllers.js"
 import UserController
     from "./controllers/users/users-controller.js"
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
 
 const app = express()
 app.use(express.json());
 HelloController(app)
+TuitsController(app)
 UserController(app)
 app.listen(4000)
