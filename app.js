1e2be158ef885183bbd0104f83d6a569e4f2a3bb
AiyappaND <vinayaiyappa24816@gmail.com>
2023-04-01 21:37:54 -0400

    Node required lab

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import HelloController
+    from "./controllers/hello-controllers.js"
+import UserController
+    from "./controllers/users/users-controller.js"
+
+const app = express()
+app.use(express.json());
+HelloController(app)
+UserController(app)
+app.listen(4000)
